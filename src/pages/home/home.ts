7c56eb86af426c1102d5182e7a5a8de5134f391c
KeyangXiang <keyang.xiang@gmail.com>
2017-02-27 18:42:50 +0000

    changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,18 +33,13 @@ export class HomePage {
   }
 
   calc() {
-
     const total = parseFloat(this.num1) + parseFloat(this.num2);
-    if (isNaN(total)) {
-      console.error(`not valid input. num1: ${this.num1} num2: ${this.num2}`);
-    } else {
-      console.log(`Math result ${total}`);
-      this.alertCtrl.create({
-        title: 'Math result',
-        subTitle: `${this.num1} + ${this.num2} = ${total}`,
-        buttons: ['OK']
-      }).present();
-    }
+    console.log(`Math result ${total}`);
+    this.alertCtrl.create({
+      title: 'Math result',
+      subTitle: `${this.num1} + ${this.num2} = ${total}`,
+      buttons: ['OK']
+    }).present();
   }
 
 }
