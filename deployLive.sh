6f6c11b0ad3f756a54acdf5260a6a2b1ddaff606
KeyangXiang <keyang.xiang@gmail.com>
2017-02-27 12:25:58 +0000

    update home screen

--- deployLive.sh
+++ deployLive.sh
@@ -2,3 +2,6 @@
 cordova-hcp build
 rm -Rf ./liveUpdate/public/* 
 cp -R ./www/* ./liveUpdate/public/
+cd ./liveUpdate
+git commit -am "live changes"
+git push dsb master --force
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,50 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private _num1: string;
 
-  constructor(public navCtrl: NavController) {
+  set num1(val) {
+    console.log(`set number 1 to ${val}`)
+    this._num1 = val;
+  }
+  get num1() {
+    return this._num1;
+  }
 
+  private _num2: string;
+
+  set num2(val) {
+    console.log(`set number 2 to ${val}`)
+    this._num2 = val;
+  }
+  get num2() {
+    return this._num2;
+  }
+  constructor(
+    public navCtrl: NavController,
+    private alertCtrl: AlertController
+  ) {
+  }
+
+  calc() {
+
+    const total = parseFloat(this.num1) + parseFloat(this.num2);
+    if (isNaN(total)) {
+      console.error(`not valid input. num1: ${this.num1} num2: ${this.num2}`);
+    } else {
+      console.log(`Math result ${total}`);
+      this.alertCtrl.create({
+        title: 'Math result',
+        subTitle: `${this.num1} + ${this.num2} = ${total}`,
+        buttons: ['OK']
+      }).present();
+    }
   }
 
 }
